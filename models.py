edf473473aee049750a6a31ce8e33cfb9741de82
Lycoriss <167539269+Parkchanhee123@users.noreply.github.com>
2025-06-04 10:00:21 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim=16000, output_dim=2):
         super().__init__()
         self.fc = nn.Sequential(
-            nn.Linear(100, 64),
+            nn.Linear(input_dim, 256),
             nn.ReLU(),
-            nn.Linear(64, 2)
+            nn.Linear(256, output_dim)
         )
 
     def forward(self, x):
